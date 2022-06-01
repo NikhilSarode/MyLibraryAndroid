51c67a5a3da14758502ff3306cf3932505b3d9c1
NikhilSarode <nikhilsarode29@yahoo.in>
2022-06-01 21:53:44 +0530

    animations and action toolbar

--- app/src/main/java/com/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/mylibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.mylibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -19,13 +21,37 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+       // overridePendingTransition(R.anim.slide_in,R.anim.slide_out);  //animation
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //to enable back button in action bar
+
         booksRecyclerView = findViewById(R.id.booksRecyclerView);
         adapter=new BookRecViewAdapter(this,"allBooks");
 
         booksRecyclerView.setAdapter(adapter);
         booksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
         adapter.setBooks(Utils.getInstance().getAllBooks());
+    }
+
+    //to make the animation work on pressing ack button
+    //this callback is called when activity finishes
+    /*@Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_out,R.anim.slide_in);
+    }*/
+
+    //on clicking button in action menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
